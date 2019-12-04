a055586ec08e235ce934be797144fa47b9e12eba
JoshuaTaylorBW <joshuataylorbw@gmail.com>
2019-12-04 15:31:21 -0700

    Wall Color Manager Pre Implementation

---
+++ ChangeWallColorForLevel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeWallColorForLevel : MonoBehaviour
+{
+
+    public WallColorManager wcm; 
+    public MeshRenderer meshRenderer; 
+
+    public Material newMaterialOne;
+    public Material newMaterialTwo;
+
+    public Material[] mats;
+
+    void Start()
+    {
+        wcm = GameObject.Find("Wall Color Manager").GetComponent<WallColorManager>();
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        newMaterialOne = wcm.GetCurrentTopAndBottomWallMaterial();
+        newMaterialTwo = wcm.GetCurrentMiddleWallMaterial();
+
+
+
+        if(wcm.ShouldChangeWallMaterials()) {
+            Debug.Log(newMaterialOne);
+            mats = meshRenderer.materials;
+            mats[0] = newMaterialTwo; 
+            mats[1] = newMaterialOne; 
+            meshRenderer.materials = mats;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ WallColorManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallColorManager : MonoBehaviour
+{
+
+    [Header("Stage Two")]
+    public Material stageTwoTopAndBottomMaterial;
+    public Material stageTwoMiddleMaterial;
+    [Header("Stage Three")]
+    public Material stageThreeTopAndBottomMaterial;
+    public Material stageThreeMiddleMaterial;
+    [Header("Stage Four")]
+    public Material stageFourTopAndBottomMaterial;
+    public Material stageFourMiddleMaterial;
+    [Header("Stage Five")]
+    public Material stageFiveTopAndBottomMaterial;
+    public Material stageFiveMiddleMaterial;
+
+    private LevelUpManager lum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       lum = GameObject.Find("Level Up Manager").GetComponent<LevelUpManager>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool ShouldChangeWallMaterials() {
+       return lum.GetCurrentStage() > 1;
+    }
+
+    public Material GetCurrentTopAndBottomWallMaterial() {
+        switch(lum.GetCurrentStage()) {
+            case 2: 
+                return stageTwoTopAndBottomMaterial;
+            case 3: 
+                return stageThreeTopAndBottomMaterial;
+            case 4: 
+                return stageFourTopAndBottomMaterial;
+            case 5: 
+                return stageFiveTopAndBottomMaterial;
+        }
+
+        return stageTwoMiddleMaterial;
+
+    }
+
+    public Material GetCurrentMiddleWallMaterial() {
+        switch(lum.GetCurrentStage()) {
+            case 2: 
+                return stageTwoMiddleMaterial;
+            case 3: 
+                return stageThreeMiddleMaterial;
+            case 4: 
+                return stageFourMiddleMaterial;
+            case 5: 
+                return stageFiveMiddleMaterial;
+        }
+
+        return stageTwoMiddleMaterial;
+    }
+
+}
