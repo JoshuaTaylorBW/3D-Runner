bd543654e119963ee337f3fb56f55bf23f8f971a
JoshuaTaylorBW <joshuataylorbw@gmail.com>
2019-12-10 10:56:50 -0700

    Add money to blocks.

---
+++ Scripts/RandomLeftRightObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomLeftRightObject : MonoBehaviour
+{
+
+    private bool onTheLeft;
+    public bool shouldSwitchOtherObjectPlacement;
+    public GameObject objectToSwitch;
+    [Header("Object to switch properties")]
+    public bool switchToOppositeSide;
+
+    void Start()
+    {
+        onTheLeft = Random.Range(0, 2) == 0 ? true : false;        
+
+        transform.position = new Vector3(onTheLeft ? -90 : 90, transform.position.y, transform.position.z);
+
+        if(shouldSwitchOtherObjectPlacement) {
+            if(switchToOppositeSide) {
+                objectToSwitch.transform.position = new Vector3(onTheLeft ? 90 : -90, objectToSwitch.transform.position.y, objectToSwitch.transform.position.z);
+            }else{
+                objectToSwitch.transform.position = new Vector3(onTheLeft ? -90 : 90, objectToSwitch.transform.position.y, objectToSwitch.transform.position.z);
+            }
+        }
+    }
+}
